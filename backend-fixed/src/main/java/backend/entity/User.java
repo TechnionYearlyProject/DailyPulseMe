67a8b3c7a38d89f9be4c9cbd6c6920ab2cb9ed30
Rober Shhock <rober_sh@campus.technion.ac.il>
2017-12-02 20:51:48 +0200

    fixed2

---
+++ backend-fixed/src/main/java/backend/entity/User.java
@@ -0,0 +1,67 @@
+package backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Document
+public class User{
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    @JsonIgnore
+    private String password;
+
+    public Set<Role> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<Role> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    private Set<Role> userRoles = new HashSet<>();
+
+    public User(String email, String name, String password) {
+        this.password = password;
+        //this.roles = roles;
+        this.email = email;
+        this.name = name;
+    }
+
+    User() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
