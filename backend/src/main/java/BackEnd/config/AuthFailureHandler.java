e9bdfebedc22d003317551c16fd8f50c1ed9043a
mohamad <abdalazez@campus.technion.ac.il>
2017-12-06 21:47:51 +0200

    deleted all the unnecessary files

--- backend/src/main/java/BackEnd/config/AuthFailureHandler.java
+++
@@ -1,9 +0,0 @@
-package BackEnd.config;
-
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.stereotype.Component;
-
-@Component
-public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler{
-	
-}
