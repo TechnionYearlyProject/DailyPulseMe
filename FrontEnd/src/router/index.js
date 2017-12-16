73dbda0520255c820e6a0eb960e9568b5a432927
Najeeb Nabwani <najeeb@campus.technion.ac.il>
2017-12-16 16:36:17 +0200

    updating but still not working

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,14 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '../components/HelloWorld'
 import Login from '../components/Login'
-import Register from '../components/Register'
+import store from '../store'
+import Register from '../components/register'
+import * as types from '../store/mutation-types'
+
+const hasToken = (to, from, next) => {
+  const token = localStorage.getItem('JWT')
+  const username = localStorage.getItem('username')
+  if (token) {
+    store.commit(types.LOGIN_SUCCESS, { token, username })
+    router.push('/home')
+  } else {
+    next()
+  }
+}
+
+const requireAuth = (to, from, next) => {
+  if (store.getters.isLoggedIn) {
+    next()
+  } else {
+    router.push('/')
+  }
+}
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
+const router = new Router({
   routes: [
-      { path: '/', component: Login },
-      { path: '/register', component: Register }
+    {
+      path: '/',
+      alias: '/login',
+      name: 'Login',
+      component: Login,
+      beforeEnter: hasToken
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    }
   ]
 })
+
+export default router
