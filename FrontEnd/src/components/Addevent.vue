44d57480015d2dc4ea7a3e7c37148fa0063b5c58
yotam <yotamsh@campus.technion.ac.il>
2018-01-05 20:43:34 +0200

    addevent frontend

--- FrontEnd/src/components/Addevent.vue
+++ FrontEnd/src/components/Addevent.vue
@@ -1,45 +1,94 @@
 <template>
   <div style="width:50%; margin:auto; margin-top: 20px;">
-    <b-form>
+    <b-form @submit.prevent="addevent">
       <b-form-group id="exampleInputGroup1"
-                    label="Email address:"
-                    label-for="exampleInput1"
-                    description="We'll never share your email with anyone else.">
-        <b-form-input id="exampleInput1"
-                      type="email"
-                      required
-                      placeholder="Enter email">
-        </b-form-input>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup2"
-                    label="Your Name:"
-                    label-for="exampleInput2">
-        <b-form-input id="exampleInput2"
+                    label="Event Name:"
+                    label-for="exampleInput1">
+        <b-form-input v-model="input.name"
                       type="text"
                       required
                       placeholder="Enter name">
         </b-form-input>
       </b-form-group>
-      <b-form-group id="exampleInputGroup3"
-                    label="Food:"
-                    label-for="exampleInput3">
-        <b-form-select id="exampleInput3"
-                      required>
-        </b-form-select>
+      <b-form-group label="Date:"
+                    label-for="exampleInput1">
+        <b-form-input v-model="date"
+                      type="date"
+                      required
+                      placeholder="Enter date">
+        </b-form-input>
+        {{this.date}}
       </b-form-group>
-      <b-form-group id="exampleGroup4">
-        <b-form-checkbox-group id="exampleChecks">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
+      <b-form-group label="Start time:"
+                    label-for="exampleInput1">
+        <b-form-input v-model="starttime"
+                      type="time"
+                      required
+                      placeholder="Enter start">
+        </b-form-input>
+      </b-form-group>
+      {{this.starttime}}
+
+      <b-form-group label="End time:"
+                    label-for="exampleInput1">
+        <b-form-input v-model="endtime"
+                      type="time"
+                      required
+                      placeholder="Enter end">
+        </b-form-input>
+      </b-form-group>
+      {{this.endtime}}
+
+      <b-form-group label="Description:"
+                    label-for="exampleInput2">
+        <b-form-input v-model="input.description"
+                      type="text"
+                      required
+                      placeholder="Enter description">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup3"
+                    label="Tag:"
+                    label-for="exampleInput3">
+        <b-form-input v-model="input.tag"
+                      type="text"
+                      required
+                      placeholder="Enter tag">
+        </b-form-input>
       </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
   </div>
 </template>
-<script>
-export default {
-  name: 'Profile'
-}
+<script type="application/json">
+  export default{
+    name: 'Event',
+    data () {
+      return {
+        date: '',
+        starttime: '',
+        endtime: '',
+        input: {
+          name: '',
+          start: this.date + 'D' + this.starttime,
+          end: this.date + 'D' + this.endtime,
+          description: '',
+          tag: ''
+        }
+      }
+    },
+    methods: {
+      addevent() {
+        let url = 'http://localhost:8081/users/addEvent'
+        this.$http.post(url, this.input,{headers: {'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('token')}} ).then((res) => {
+          console.log('Success', res);
+          //this.$router.push('/');
+        }, (err) => {
+          console.log('Error: ', err)
+        })
+      }
+    }
+  }
 </script>
