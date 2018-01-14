263dd233cd0f3d2d170c24324fbaa893a5213134
yotam <yotamsh@campus.technion.ac.il>
2018-01-14 20:44:42 +0200

    avarage heart rate

---
+++ FrontEnd/src/components/avarge_heartrate.js
@@ -0,0 +1,30 @@
+function heartStats(rate, age){
+  if(age < 2){
+    if(rate <= 100){
+      return "bellow avarage";
+    }
+    if(rate >= 170){
+      return "above avarage";
+    }
+  }
+  else if(age > 1 && age <= 11){
+    if(rate <= 60){
+      return "bellow avarage";
+    }
+    if(rate >= 110){
+      return "above avarage";
+    }
+  }
+  else if(age > 11){
+    if(rate < 40){
+      return "dangerous";
+    }
+    if(rate < 60 && rate >= 40){
+      return "athlete";
+    }
+    if(rate > 90){
+      return "above avarage";
+    }
+  }
+  return "avarage";
+}
