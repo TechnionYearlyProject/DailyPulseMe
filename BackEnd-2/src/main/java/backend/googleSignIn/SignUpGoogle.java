2ff699b7384beba273e44cc4bda138cffb7baf8a
mohamad <abdalazez@campus.technion.ac.il>
2018-04-19 20:47:33 +0300

    implemented a user sign up in the backend issue number : #77

---
+++ BackEnd-2/src/main/java/backend/googleSignIn/SignUpGoogle.java
@@ -0,0 +1,73 @@
+package backend.googleSignIn;
+import com.google.api.client.googleapis.auth.oauth2.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+
+public class SignUp {
+    String CLIENT_SECRET_FILE = "/src/main/java/resources/client_secret.json";
+    String redirectUrl="http://localhost:8080/token";
+    String authCode="we have to take it from frontend";
+
+    GoogleClientSecrets clientSecrets;
+
+    {
+        try {
+            clientSecrets = GoogleClientSecrets.load(
+                    JacksonFactory.getDefaultInstance(), new FileReader(CLIENT_SECRET_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    GoogleTokenResponse tokenResponse;
+
+    {
+        try {
+            tokenResponse = new GoogleAuthorizationCodeTokenRequest(
+                    new NetHttpTransport(),
+                    JacksonFactory.getDefaultInstance(),
+                    "https://www.googleapis.com/oauth2/v4/token",
+                    clientSecrets.getDetails().getClientId(),
+                    clientSecrets.getDetails().getClientSecret(),
+                    authCode,
+                    redirectUrl)  // Specify the same redirect URI that you use with your web
+                    // app. If you don't have a web version of your app, you can
+                    // specify an empty string.
+                    .execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String accessToken = tokenResponse.getAccessToken();
+
+    // Use access token to call API
+    GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
+    // Get profile info from ID token
+    GoogleIdToken idToken;
+
+    {
+        try {
+            idToken = tokenResponse.parseIdToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        GoogleIdToken.Payload payload = idToken.getPayload();
+        String userId = payload.getSubject();  // Use this value as a key to identify a user.
+        String email = payload.getEmail();
+        boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+        String name = (String) payload.get("name");
+        String pictureUrl = (String) payload.get("picture");
+        String locale = (String) payload.get("locale");
+        String familyName = (String) payload.get("family_name");
+        String givenName = (String) payload.get("given_name");
+
+}
