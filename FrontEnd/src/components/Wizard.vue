f4b6b91192c69b7f9ca80a4e40328e1be4c8beab
Peleg <pelegyair@campus.technion.ac.il>
2018-04-21 19:32:39 +0300

    UI - added a wizard - not complete yet. Added Microsoft login - there is a problem with cors but it is posibble to get a token.

---
+++ FrontEnd/src/components/Wizard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="width:50%;margin: auto;">
+  <form-wizard @on-complete="onComplete" 
+                        shape="square"
+                        color="#3498db"
+                        title="DailyPulse"
+                        subtitle="A few steps are required">
+      <tab-content title="Fitness Band Account"
+                   icon="ti-user">
+                   <b-btn v-b-toggle.collapse1 variant="success" v-on:click="microsoftfit">Microsoft</b-btn>
+    <b-btn v-b-toggle.collapse1 variant="warning" v-on:click="googlefit" >Google</b-btn>
+      </tab-content>
+      <tab-content title="Calendar Account"
+                   icon="ti-settings">
+        <b-btn v-b-toggle.collapse1 variant="success" >Fitbit</b-btn>
+    <b-btn v-b-toggle.collapse1 variant="warning" >Google Fit</b-btn>
+      </tab-content>
+      <tab-content title="Last step"
+                   icon="ti-check">
+        Yuhuuu! This seems pretty damn simple
+      </tab-content>
+  </form-wizard>
+</div>
+</template>
+<script src="https://unpkg.com/vue-form-wizard/dist/vue-form-wizard.js"></script>
+
+<script>
+  import Connect from './Connect'
+export default {
+  components:{ Connect },
+    name:'Wizard',
+     methods: {
+      onComplete: function(){
+          alert('Yay. Done!');
+       }
+      }
+  }
+</script>
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -11,10 +11,14 @@ import VeeValidate from 'vee-validate';
 Vue.use(VeeValidate);
 Vue.use(BootstrapVue);
 Vue.use(VueResource)
+import VueFormWizard from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+Vue.use(VueFormWizard)
 Vue.http.options.emulateJSON = true
 global.jQuery = jQuery
 // require('bootstrap')
 Vue.config.productionTip = false
+
 /* eslint-disable no-new */
 new Vue({  // eslint-disable-line no-new
   el: '#app',
