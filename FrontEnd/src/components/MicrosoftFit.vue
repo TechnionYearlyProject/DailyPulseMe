4f8372eb6a729686315719333e9519241e88a8e1
Peleg <pelegyair@campus.technion.ac.il>
2018-04-26 01:17:06 +0300

    Front end:microsoft token is working now! not sending it to DB yet. Wizard is almost complete. (integrated microsoft&google login into wizard).
    Signed-off-by: Peleg <pelegyair@campus.technion.ac.il>

--- FrontEnd/src/components/MicrosoftFit.vue
+++ FrontEnd/src/components/MicrosoftFit.vue
@@ -12,79 +12,24 @@ export default {
       succesful: false
     }
   },created: function () {
-    // console.log(this.succesful)
-    if(this.succesful){
-      console.log('second time ?')
-      // this.$http.post('http://localhost:8081/users/updataGoogleFitToken',{
-      //         "first": this.accessToken.toString() ,
-      //         "second": this.refreshToken.toString()
-      //       }
-      //        ,{headers: {'Content-Type': 'application/json',
-      //         'Authorization': localStorage.getItem('token'),}
-      //       }).then((res) =>{
-      //         console.log(res);
-      //         console.log(successsssss);
-      //       },(err) =>{
-      //         console.log(err);
-      //         console.log(faillllll);
-      //       })
-      //       console.log('hi')
-      //           window.location.href = "http://localhost:8080/config/";
-      //           // router.replace('/');
-      // // location.assign('http://localhost:8080/config/');
-    }
     let route = this.$route.fullPath;
     var x = route.split('=')[1];
     this.accessToken = decodeURIComponent(x);
-     this.$http.post('http://localhost:8081/users/getOutlookToken',{
+    this.$http.post('http://localhost:8081/users/getOutlookToken',{
         "first": this.accessToken,
         "second": ''
       }
        ,{headers: {'Content-Type': 'application/json',
         'Authorization': localStorage.getItem('token'),}
       }).then((res) =>{
-        this.$router.push('/');
+        
         console.log(res);
       },(err) =>{
+        close();  
         console.log(err);
       })
     // this.getTokens();
-  },
-   methods : {
-     getTokens() {
 
-       let url = 'https://login.microsoftonline.com/common/oauth2/token/'
-       this.$http.post(url, {
-        client_id: 'c8b9175b-e478-4c52-b8e6-178246c03006',
-        response_type:'token',
-        code: this.accessCode,
-        redirect_uri: 'http://localhost:8080/token1',
-        grant_type: 'authorization_code',
-        resource: '00000003-0000-0000-c000-000000000000',
-        client_secret: 'xlgsIAORXV4_njfK0325((^'}).then((res)=>{
-        console.log("hi")
-        if(res.ok != 'false'){
-       // this.$http.post('http://localhost:8081/users/updateGoogleFitToken',{
-       //        "first": res.body.access_token.toString() ,
-       //        "second": res.body.refresh_token.toString()
-       //      }
-       //       ,{headers: {'Content-Type': 'application/json',
-       //        'Authorization': localStorage.getItem('token'),}
-       //      }).then((res) =>{
-       //        this.$router.push('/');
-       //        console.log(res);
-       //      },(err) =>{
-       //        console.log(err);
-       //      })
-          }
-       // this.accessToken = res.body.access_token;
-       // this.refreshToken = res.body.refresh_token;
-       this.succesful = true;
-            },(err) =>{
-       console.log("no");
-     });
-        // console.log(this.succesful)
-     }
   }
 }
 </script>
