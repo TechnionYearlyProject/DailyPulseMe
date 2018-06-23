6a3859c16e8526a7f7a2f36f96aca7dd029f942e
yotam <yotamsh@campus.technion.ac.il>
2018-06-24 01:11:48 +0300

    Validation of email

--- FrontEnd/src/components/Register.vue
+++ FrontEnd/src/components/Register.vue
@@ -47,6 +47,26 @@
       toggleLoginType () {
         this.loginType === 'username' ? this.loginType = 'email' : this.loginType = 'username'
       },
+	validateEmail(email) {
+		  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		  return re.test(email);
+		},
+
+		validate() {
+		  var $result = $("#result");
+		  var email = $("#email").val();
+		  $result.text("");
+
+		  if (validateEmail(email)) {
+			$result.text(email + " is valid :)");
+			$result.css("color", "green");
+		  } else {
+			$result.text(email + " is not valid :(");
+			$result.css("color", "red");
+		  }
+		  return false;
+		},
+
       register () {
         if (this.user.password.length < 6){
           this.errMsg = 'Password Length Must Be At Least 6'
